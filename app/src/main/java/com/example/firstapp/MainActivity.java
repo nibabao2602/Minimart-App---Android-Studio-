f092ee01944cfc38b11bd5f76c122723198429e0
nibabao2602 <nhilx150017@gmail.com>
2021-05-13 09:44:16 -0700

    Add Project

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Tools.setSystemBarLight(this);
+        Tools.setSystemBarColor(this,R.color.white);
+
+        // catch Element
+        TextView signIn = (TextView)findViewById(R.id.btnSignIn);
+        TextView signUp = (TextView)findViewById(R.id.btnSignUp);
+        TextView forgotPass = (TextView)findViewById(R.id.btnForgotPass);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
